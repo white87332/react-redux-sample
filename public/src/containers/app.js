173cfc5fd03abda4583c1a245239d07ad9725133
Tony Jiang <white87332@gmail.com>
2016-05-19 12:25:38 +0800

    update

--- public/src/containers/app.js
+++ public/src/containers/app.js
@@ -11,7 +11,7 @@ const store = configureStore();
 // react-router-redux
 const history = syncHistoryWithStore(browserHistory, store);
 
-// lazy load component
+// async load component
 const loadConmponentAsync = bundle => (location, callback) =>
 {
 	bundle(component => {
@@ -22,16 +22,16 @@ const loadConmponentAsync = bundle => (location, callback) =>
 const routes = (
 	<Router history={history}>
 		<Route getComponent={loadConmponentAsync(require('bundle?lazy&name=layout!../components/layout/layout'))}>
-			<Route  path= "/selectItems" getComponent={loadConmponentAsync(require('bundle?lazy&name=selectItems!../components/selectItems/selectItems'))} />
-			<Route  path= "/drag" getComponent={loadConmponentAsync(require('bundle?lazy&name=drag!../components/drag/drag'))} />
-			<Route  path= "/dnd" getComponent={loadConmponentAsync(require('bundle?lazy&name=board!../components/dnd/dnd'))} />
-			<Route  path= "/transMotion" getComponent={loadConmponentAsync(require('bundle?lazy&name=transMotion!../components/transMotion/transMotion'))} />
-			<Route  path= "/sortable" getComponent={loadConmponentAsync(require('bundle?lazy&name=sortable!../components/sortable/sortable'))} />
-			<Route  path= "/carousel" getComponent={loadConmponentAsync(require('bundle?lazy&name=carousel!../components/carousel/carousel'))} />
-			<Route  path= "/draggableBalls" getComponent={loadConmponentAsync(require('bundle?lazy&name=draggableBalls!../components/draggableBalls/draggableBalls'))} />
-			<Route  path= "/posts" getComponent={loadConmponentAsync(require('bundle?lazy&name=posts!../components/posts/posts'))} />
-			<Route  path= "/counter" getComponent={loadConmponentAsync(require('bundle?lazy&name=counter!../components/counter/counter'))} />
-			<Route  path= "/donut" getComponent={loadConmponentAsync(require('bundle?lazy&name=donut!../components/donut/donutEach'))} />
+			<Route path= "/selectItems" getComponent={loadConmponentAsync(require('bundle?lazy&name=selectItems!../components/selectItems/selectItems'))} />
+			<Route path= "/drag" getComponent={loadConmponentAsync(require('bundle?lazy&name=drag!../components/drag/drag'))} />
+			<Route path= "/dnd" getComponent={loadConmponentAsync(require('bundle?lazy&name=board!../components/dnd/dnd'))} />
+			<Route path= "/transMotion" getComponent={loadConmponentAsync(require('bundle?lazy&name=transMotion!../components/transMotion/transMotion'))} />
+			<Route path= "/sortable" getComponent={loadConmponentAsync(require('bundle?lazy&name=sortable!../components/sortable/sortable'))} />
+			<Route path= "/carousel" getComponent={loadConmponentAsync(require('bundle?lazy&name=carousel!../components/carousel/carousel'))} />
+			<Route path= "/draggableBalls" getComponent={loadConmponentAsync(require('bundle?lazy&name=draggableBalls!../components/draggableBalls/draggableBalls'))} />
+			<Route path= "/posts" getComponent={loadConmponentAsync(require('bundle?lazy&name=posts!../components/posts/posts'))} />
+			<Route path= "/counter" getComponent={loadConmponentAsync(require('bundle?lazy&name=counter!../components/counter/counter'))} />
+			<Route path= "/donut" getComponent={loadConmponentAsync(require('bundle?lazy&name=donut!../components/donut/donutEach'))} />
 		</Route>
     </Router>
 );
