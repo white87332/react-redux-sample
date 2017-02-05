c1a321f695236307697d0b0b1561b7d340b5eea9
Tony Jiang <white87332@gmail.com>
2017-02-05 11:02:41 +0800

    update webpack2 prod config

--- public/src/routes/routes.js
+++ public/src/routes/routes.js
@@ -27,21 +27,20 @@ export default function createRoutes(history, store, dispatch)
     return (
         <Router history={history}>
 			<Route component={require('../components/main/main').default}>
-				<Route path= "/" getComponent={loadConmponentAsync(require('bundle?lazy!../components/counter/counter'))} />
-	    		<Route getComponent={loadConmponentAsync(require('bundle?lazy!../components/layout/layout'))}>
-	    			<Route path= "/selectItems" getComponent={loadConmponentAsync(require('bundle?lazy!../components/selectItems/selectItems'))} />
-	    			<Route path= "/drag" getComponent={loadConmponentAsync(require('bundle?lazy!../components/drag/drag'))} />
-	    			<Route path= "/dnd" getComponent={loadConmponentAsync(require('bundle?lazy!../components/dnd/dnd'))} />
-	    			<Route path= "/transMotion" getComponent={loadConmponentAsync(require('bundle?lazy!../components/transMotion/transMotion'))} />
-	    			<Route path= "/sortable" getComponent={loadConmponentAsync(require('bundle?lazy!../components/sortable/sortable'))} />
-	    			<Route path= "/carousel" getComponent={loadConmponentAsync(require('bundle?lazy!../components/carousel/carousel'))} />
-	    			<Route path= "/draggableBalls" getComponent={loadConmponentAsync(require('bundle?lazy!../components/draggableBalls/draggableBalls'))} />
-	    			<Route path= "/posts" getComponent={loadConmponentAsync(require('bundle?lazy!../components/posts/posts'))} onEnter={postsListInit(store, dispatch)}/>
-	    			<Route path= "/counter" getComponent={loadConmponentAsync(require('bundle?lazy!../components/counter/counter'))} />
-	    			<Route path= "/donut" getComponent={loadConmponentAsync(require('bundle?lazy!../components/donut/donut'))} />
-					<Route path= "/slider" getComponent={loadConmponentAsync(require('bundle?lazy!../components/slider/slider'))} />
-					<Route path= "/areaChart" getComponent={loadConmponentAsync(require('bundle?lazy!../components/areaChart/areaChart'))} />
-					<Route path= "/calendar" getComponent={loadConmponentAsync(require('bundle?lazy!../components/calendar/calendar'))} />
+				<Route path= "/" getComponent={loadConmponentAsync(require('bundle?name=counter&lazy!../components/counter/counter'))} />
+	    		<Route getComponent={loadConmponentAsync(require('bundle?name=layout&lazy!../components/layout/layout'))}>
+	    			<Route path= "/selectItems" getComponent={loadConmponentAsync(require('bundle?name=selectItems&lazy!../components/selectItems/selectItems'))} />
+	    			<Route path= "/drag" getComponent={loadConmponentAsync(require('bundle?name=drag&lazy!../components/drag/drag'))} />
+	    			<Route path= "/dnd" getComponent={loadConmponentAsync(require('bundle?name=dnd&lazy!../components/dnd/dnd'))} />
+	    			<Route path= "/transMotion" getComponent={loadConmponentAsync(require('bundle?name=transMotion&lazy!../components/transMotion/transMotion'))} />
+	    			<Route path= "/sortable" getComponent={loadConmponentAsync(require('bundle?name=sortable&lazy!../components/sortable/sortable'))} />
+	    			<Route path= "/draggableBalls" getComponent={loadConmponentAsync(require('bundle?name=draggableBalls&lazy!../components/draggableBalls/draggableBalls'))} />
+	    			<Route path= "/posts" getComponent={loadConmponentAsync(require('bundle?name=posts&lazy!../components/posts/posts'))} onEnter={postsListInit(store, dispatch)}/>
+	    			<Route path= "/counter" getComponent={loadConmponentAsync(require('bundle?name=counter&lazy!../components/counter/counter'))} />
+	    			<Route path= "/donut" getComponent={loadConmponentAsync(require('bundle?name=donut&lazy!../components/donut/donut'))} />
+					<Route path= "/slider" getComponent={loadConmponentAsync(require('bundle?name=slider&lazy!../components/slider/slider'))} />
+					<Route path= "/areaChart" getComponent={loadConmponentAsync(require('bundle?name=areaChart&lazy!../components/areaChart/areaChart'))} />
+					<Route path= "/calendar" getComponent={loadConmponentAsync(require('bundle?name=calendar&lazy!../components/calendar/calendar'))} />
 	    		</Route>
 			</Route>
         </Router>
