92372d18ce8c07c48628127802da73138545a6bc
Tony Jiang <white87332@gmail.com>
2017-02-22 15:49:42 +0800

    add prerender

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
-var path = require('path');
-var express = require('express');
-var webpack = require('webpack');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
-var config = require('./webpack.development.config');
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.development.config');
 
-var app = express();
-var port = 3000;
+const app = express();
+const port = 3000;
 
-var compiler = webpack(config);
+const compiler = webpack(config);
 
 app.use(express.static(path.resolve('public')));
 
 if (process.env.NODE_ENV !== 'production')
 {
     app.use(webpackDevMiddleware(compiler,
-    {
-        noInfo: true,
-        publicPath: config.output.publicPath
-    }));
+        {
+            noInfo: true,
+            publicPath: config.output.publicPath
+        }));
     app.use(webpackHotMiddleware(compiler));
 }
+
+app.use(require('prerender-node'));
 
 app.get('*', (req, res) =>
 {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, (error) =>
+app.listen(port, () =>
 {
-    if (error)
-    {
-        console.error(error);
-    }
-    else
-    {
-        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
-    }
+    console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
 });
--- public/src/containers/app.js
+++ public/src/containers/app.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import { syncHistoryWithStore } from 'react-router-redux';
 import { render } from 'react-dom';
-import { browserHistory, match } from 'react-router';
+import { browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
-import configureStore from '../store/configureStore.js';
-import { syncHistoryWithStore } from 'react-router-redux';
+import configureStore from '../store/configureStore';
 import createRoutes from '../routes/routes';
 import i18n from '../i18n/i18n';
 
 if (process.env.NODE_ENV !== 'production')
 {
- 	window.Perf = require('react-addons-perf');
+    window.Perf = require('react-addons-perf');
 }
 
 // store
@@ -23,10 +23,10 @@ const history = syncHistoryWithStore(browserHistory, store);
 const routes = createRoutes(history, store, store.dispatch);
 
 render(
-	<Provider store={store}>
+    <Provider store={store}>
         <I18nextProvider i18n={i18n}>
             {routes}
         </I18nextProvider>
-	</Provider>,
+    </Provider>,
 	document.getElementById('root')
 );
