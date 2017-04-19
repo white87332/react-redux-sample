1cb11fb36bd70e062cc04a0403b159d02beee555
Tony Jiang <white87332@gmail.com>
2017-04-19 09:42:59 +0800

    move path to containers

--- public/src/routes/routes.js
+++ public/src/routes/routes.js
@@ -28,19 +28,19 @@ export default function createRoutes(history, store, dispatch)
     return (
         <Router history={history}>
             <Route component={require('../components/main/main').default}>
-                <Route path="/" getComponent={async(require('bundle?name=counter&lazy!../path/counter/counter'))} />
+                <Route path="/" getComponent={async(require('bundle?name=counter&lazy!../containers/counter/counter'))} />
                 <Route getComponent={async(require('bundle?name=layout&lazy!../components/layout/layout'))}>
-                    <Route path="/selectItems" getComponent={async(require('bundle?name=selectItems&lazy!../path/selectItems/selectItems'))} />
-                    <Route path="/drag" getComponent={async(require('bundle?name=drag&lazy!../path/drag/drag'))} />
-                    <Route path="/dnd" getComponent={async(require('bundle?name=dnd&lazy!../path/dnd/dnd'))} />
-                    <Route path="/transMotion" getComponent={async(require('bundle?name=transMotion&lazy!../path/transMotion/transMotion'))} />
-                    <Route path="/sortable" getComponent={async(require('bundle?name=sortable&lazy!../path/sortable/sortable'))} />
-                    <Route path="/draggableBalls" getComponent={async(require('bundle?name=draggableBalls&lazy!../path/draggableBalls/draggableBalls'))} />
-                    <Route path="/posts" getComponent={async(require('bundle?name=posts&lazy!../path/posts/posts'))} onEnter={postsListInit(store, dispatch)} />
-                    <Route path="/donut" getComponent={async(require('bundle?name=donut&lazy!../path/donut/donut'))} />
-                    <Route path="/slider" getComponent={async(require('bundle?name=slider&lazy!../path/slider/slider'))} />
-                    <Route path="/areaChart" getComponent={async(require('bundle?name=areaChart&lazy!../path/areaChart/areaChart'))} />
-                    <Route path="/calendar" getComponent={async(require('bundle?name=calendar&lazy!../path/calendar/calendar'))} />
+                    <Route path="/selectItems" getComponent={async(require('bundle?name=selectItems&lazy!../containers/selectItems/selectItems'))} />
+                    <Route path="/drag" getComponent={async(require('bundle?name=drag&lazy!../containers/drag/drag'))} />
+                    <Route path="/dnd" getComponent={async(require('bundle?name=dnd&lazy!../containers/dnd/dnd'))} />
+                    <Route path="/transMotion" getComponent={async(require('bundle?name=transMotion&lazy!../containers/transMotion/transMotion'))} />
+                    <Route path="/sortable" getComponent={async(require('bundle?name=sortable&lazy!../containers/sortable/sortable'))} />
+                    <Route path="/draggableBalls" getComponent={async(require('bundle?name=draggableBalls&lazy!../containers/draggableBalls/draggableBalls'))} />
+                    <Route path="/posts" getComponent={async(require('bundle?name=posts&lazy!../containers/posts/posts'))} onEnter={postsListInit(store, dispatch)} />
+                    <Route path="/donut" getComponent={async(require('bundle?name=donut&lazy!../containers/donut/donut'))} />
+                    <Route path="/slider" getComponent={async(require('bundle?name=slider&lazy!../containers/slider/slider'))} />
+                    <Route path="/areaChart" getComponent={async(require('bundle?name=areaChart&lazy!../containers/areaChart/areaChart'))} />
+                    <Route path="/calendar" getComponent={async(require('bundle?name=calendar&lazy!../containers/calendar/calendar'))} />
                 </Route>
             </Route>
         </Router>
